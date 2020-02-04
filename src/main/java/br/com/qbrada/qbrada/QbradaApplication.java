05b5e37a264b7bccc2e44e741f2224ef66f7875c
Bruno Oliveira <brunooliveiraalves78@gmail.com>
2020-02-04 10:26:24 -0300

    padronizar criacao do projeto

---
+++ src/main/java/br/com/qbrada/qbrada/QbradaApplication.java
@@ -0,0 +1,13 @@
+package br.com.qbrada.qbrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbradaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/qbrada/qbrada/QbradaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.qbrada.qbrada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QbradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
