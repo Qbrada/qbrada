85aa0d5600611c1a18768674f90bf61bd0d45160
Bruno Oliveira <brunooliveiraalves78@gmail.com>
2020-02-04 13:18:16 -0300

    adicionar cadastro de evento

--- src/main/java/br/com/qbrada/qbrada/controller/QbradaController.java
+++ src/main/java/br/com/qbrada/qbrada/controller/QbradaController.java
@@ -1,13 +1,27 @@
 package br.com.qbrada.qbrada.controller;
 
+import br.com.qbrada.qbrada.model.Qbrada;
 import br.com.qbrada.qbrada.service.QbradaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class QbradaController {
 
     @Autowired
     private QbradaService service;
+
+    @GetMapping("/cadastro")
+    public String cadastrar() {
+        return "cadastro";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarEvento(Qbrada qbrada) {
+        service.cadastrarEvento(qbrada);
+        return "cadastro";
+    }
 
 }
