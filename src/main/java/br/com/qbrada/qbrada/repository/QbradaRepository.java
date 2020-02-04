54b1f64dd68b96909b7390f0dbf507ee6c63a9c3
Bruno Oliveira <brunooliveiraalves78@gmail.com>
2020-02-04 11:24:53 -0300

    adicionar @Service

--- src/main/java/br/com/qbrada/qbrada/repository/QbradaRepository.java
+++ src/main/java/br/com/qbrada/qbrada/repository/QbradaRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface QbradaRepository extends CrudRepository<Qbrada, String> {
 
-
 }
--- src/main/java/br/com/qbrada/qbrada/service/QbradaService.java
+++ src/main/java/br/com/qbrada/qbrada/service/QbradaService.java
@@ -2,7 +2,9 @@ package br.com.qbrada.qbrada.service;
 
 import br.com.qbrada.qbrada.repository.QbradaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class QbradaService {
 
     @Autowired
