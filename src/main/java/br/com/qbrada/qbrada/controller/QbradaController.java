62bfc3b08f2a6d6322eedcff49f129381105819f
Gustavo Ridgue <guferrari007@gmail.com>
2020-02-04 13:33:29 -0300

    Site com busca e cadastro de site

--- src/main/java/br/com/qbrada/qbrada/controller/QbradaController.java
+++ src/main/java/br/com/qbrada/qbrada/controller/QbradaController.java
@@ -1,9 +1,11 @@
 package br.com.qbrada.qbrada.controller;
 
+import br.com.qbrada.qbrada.model.Qbrada;
 import br.com.qbrada.qbrada.service.QbradaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class QbradaController {
@@ -12,8 +14,23 @@ public class QbradaController {
     private QbradaService service;
 
     @GetMapping
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/verEventos")
     public String procurarEventos(){
         return "procurarEventos";
+    }
+    @GetMapping("/cadastro")
+    public String cadastrar() {
+        return "cadastro";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarEvento(Qbrada qbrada) {
+        service.cadastrarEvento(qbrada);
+        return "cadastro";
     }
 
 }
--- src/main/java/br/com/qbrada/qbrada/model/Qbrada.java
+++ src/main/java/br/com/qbrada/qbrada/model/Qbrada.java
@@ -41,4 +41,136 @@ public class Qbrada {
 
     @Column
     private LocalDate dataCriacao;
+
+    @Column
+    private String rua;
+
+    @Column
+    private String estado;
+
+    @Column
+    private String bairro;
+
+    @Column
+    private int numero;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public Date getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(Date dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public String getHoraFim() {
+        return horaFim;
+    }
+
+    public void setHoraFim(String horaFim) {
+        this.horaFim = horaFim;
+    }
+
+    public String getCriador() {
+        return criador;
+    }
+
+    public void setCriador(String criador) {
+        this.criador = criador;
+    }
+
+    public String getColaboradores() {
+        return colaboradores;
+    }
+
+    public void setColaboradores(String colaboradores) {
+        this.colaboradores = colaboradores;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
