df98ccddd0669465f35f57ef482fc91819c09eda
Gustavo Ridgue <guferrari007@gmail.com>
2020-02-04 11:59:13 -0300

    Página que busca eventos por nome

--- src/main/java/br/com/qbrada/qbrada/controller/QbradaController.java
+++ src/main/java/br/com/qbrada/qbrada/controller/QbradaController.java
@@ -3,11 +3,17 @@ package br.com.qbrada.qbrada.controller;
 import br.com.qbrada.qbrada.service.QbradaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class QbradaController {
 
     @Autowired
     private QbradaService service;
+
+    @GetMapping
+    public String procurarEventos(){
+        return "procurarEventos";
+    }
 
 }
